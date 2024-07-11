42faae0df45f0ee63452ae9183185961ed13dc21
Anne <annewangui210@gmail.com>
2024-07-11 20:59:55 +0300

    phase-1-array-map-method-lab

--- index.js
+++ index.js
@@ -12,5 +12,10 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(tutorial =>{
+    const words = tutorial.split (" ");
+    const capitalizeWords = words.map(word => word.charAt(0).toUpperCase()+ word.slice(1));
+    return capitalizeWords.join(" ");
+  });
 }
+console.log(titleCased());
